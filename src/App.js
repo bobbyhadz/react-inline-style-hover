8e327d93bbc17d68f484c7704420371b8e432cb1
bobbyhadz <opmftw@gmail.com>
2024-04-07 13:37:23 +0300

    minor changes

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <div>
+      <div className="box">
+        <p>bobbyhadz.com</p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
